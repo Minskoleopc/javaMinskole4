bd19a033af41220879ad73747dc25358275ffd9a
Minskoleopc <minskoleopc@gmail.com>
2023-08-28 09:29:39 +0530

    added

---
+++ oops/day2.java
@@ -0,0 +1,129 @@
+package oops;
+
+public class day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Vehicle audi = new Vehicle();
+		
+		System.out.println(audi.color);
+		System.out.println(audi.type);
+		
+		audi.displayColor();
+		String ty = audi.displayType();
+		System.out.println(ty);
+		
+	
+
+		Vehicle audi2 = new Vehicle();
+		System.out.println(audi2.color);
+		System.out.println(audi2.type);
+		
+		audi2.displayColor();
+		String ty2 = audi.displayType();
+		System.out.println(ty2);
+		
+		audi2.color = "blue";
+		audi2.type = "SUV";
+		
+		audi2.displayColor();
+		String ty3 = audi2.displayType();
+		System.out.println(ty3);
+		
+		
+		Vehicle2 audi3 = new Vehicle2("SUV","purple");
+		audi3.displayColor();
+		String ty5 = audi3.displayType();
+		System.out.println(ty5);
+		
+		
+		Vehicle2 audi4 = new Vehicle2("Sedane","black");
+		audi4.displayColor();
+		String ty4 = audi4.displayType();
+		System.out.println(ty4);
+		
+		
+		
+		Vehicle3 audi5 = new Vehicle3();
+		
+		System.out.println(audi5.color);
+		System.out.println(audi5.type);
+		
+		
+		audi5.setColor("orange");
+		audi5.setType("Jeep");
+		
+		System.out.println(audi5.color);
+		System.out.println(audi5.type);
+		
+	}
+	
+	
+
+}
+
+// user defined data type
+
+
+class Vehicle {
+
+	String type = "sedane" ;
+	String color = "red"; 
+		
+	public void displayColor() {
+		System.out.println(this.color);
+	}
+
+	public String  displayType(){		
+		return  this.type;
+	}
+	
+}
+
+class Vehicle2 {
+	
+	
+	String type ;
+	String color ;
+	
+	public Vehicle2(String ty , String cl) {
+		this.type = ty;
+		this.color = cl;
+	}
+	
+	public void displayColor() {
+		System.out.println(this.color);
+	}
+
+	public String  displayType(){		
+		return  this.type;
+	}
+	
+}
+
+class Vehicle3 {
+		String color;
+		String type;
+		
+	public String setColor(String cl) {
+		this.color = cl;
+		return this.color;
+	}	
+			
+	public String setType(String ty) {
+		this.type = ty;
+		return this.type;
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
+
